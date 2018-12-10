5f6edf6a92a73dc83917693bfde9d278c808e7e6
lzy369 <1059446341@qq.com>
2018-12-10 23:10:32 +0800

    新建example-dubbo项目

--- lazy-tcc-example-dubbo/sql/lazy_tcc_shared_services_customer.sql
+++ lazy-tcc-example-dubbo/sql/lazy_tcc_shared_services_customer.sql
@@ -4,22 +4,22 @@ Navicat MySQL Data Transfer
 Source Server         : localhost
 Source Server Version : 50639
 Source Host           : localhost:3306
-Source Database       : lazy_tcc_shared_services_capital
+Source Database       : lazy_tcc_shared_services_customer
 
 Target Server Type    : MYSQL
 Target Server Version : 50639
 File Encoding         : 65001
 
-Date: 2018-12-10 23:00:55
+Date: 2018-12-10 23:06:00
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for t_capital
+-- Table structure for t_customer
 -- ----------------------------
-DROP TABLE IF EXISTS `t_capital`;
-CREATE TABLE `t_capital` (
+DROP TABLE IF EXISTS `t_customer`;
+CREATE TABLE `t_customer` (
   `id` bigint(20) NOT NULL COMMENT '主键',
   `customer_no` varchar(32) NOT NULL COMMENT '客户编号',
   `total_capital` decimal(15,3) NOT NULL COMMENT '总资金',
