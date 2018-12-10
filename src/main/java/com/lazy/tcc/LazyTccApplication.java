3bbe9e562ae55adf407c9e47009a76892a02a28f
laizhiyuan <13423803799@163.com>
2018-12-10 15:26:39 +0800

    初始化项目

---
+++ src/main/java/com/lazy/tcc/LazyTccApplication.java
@@ -0,0 +1,12 @@
+package com.lazy.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyTccApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lazy/tcc/LazyTccApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lazy.tcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LazyTccApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
