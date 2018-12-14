c2eec053df0eaef2897b7f73615b8530697cfd78
laizhiyuan <13423803799@163.com>
2018-12-14 18:28:48 +0800

    开始编写核心模块

---
+++ lazy-tcc-core/src/main/java/com/lazy/tcc/core/interceptor/GlobalTransactionInterceptor.java
@@ -0,0 +1,19 @@
+package com.lazy.tcc.core.interceptor;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>
+ * GlobalTransactionInterceptor Definition
+ * </p>
+ *
+ * @author laizhiyuan
+ * @date 2018/12/14.
+ */
+@Aspect
+@Component
+public class GlobalTransactionInterceptor {
+
+
+}
